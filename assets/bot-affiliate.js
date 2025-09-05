ddae1e9ff200b9a9a65ba4d2f2bdbfa8cd239d6f
cosemurciano <cose.murciano@gmail.com>
2025-09-05 21:38:01 +0200

    Add BotAffiliate popup settings and customization

--- assets/bot-affiliate.js
+++ assets/bot-affiliate.js
@@ -1,8 +1,19 @@
 (function($){
     $(function(){
         var box = $('#alma-bot-affiliate');
-        if(box.length){
-            box.fadeIn(600);
+        if(!box.length){
+            return;
         }
+        if (sessionStorage.getItem('almaBotAffiliateClosed')) {
+            return;
+        }
+        var anim = (typeof alma_bot_affiliate !== 'undefined' && alma_bot_affiliate.animation) ? alma_bot_affiliate.animation : 'fade';
+        box.show().addClass('alma-animation-' + anim);
+        box.on('click', '.alma-bot-affiliate-close', function(){
+            box.hide();
+            try {
+                sessionStorage.setItem('almaBotAffiliateClosed', '1');
+            } catch(e) {}
+        });
     });
 })(jQuery);
